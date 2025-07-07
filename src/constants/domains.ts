1897afaf34f9d47fa9c685c776294e36bf4a10ab
Congglee <congldqn888@gmail.com>
2025-07-07 11:30:12 +0700

    Remove the express-useragent library and add domain to the dev environment to cors

--- src/constants/domains.ts
+++ src/constants/domains.ts
@@ -1,7 +1,3 @@
 import { envConfig } from '~/config/environment'
 
-export const WHITELIST_DOMAINS: string[] = [
-  envConfig.clientUrl,
-  envConfig.googleRedirectUri,
-  'https://d362-2405-4802-1bde-e7e0-34b6-de4-ae66-d5b3.ngrok-free.app'
-]
+export const WHITELIST_DOMAINS: string[] = [envConfig.clientUrl, envConfig.clientDevUrl, envConfig.googleRedirectUri]
